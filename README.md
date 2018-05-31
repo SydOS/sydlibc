511e508a37168a6d6aab8982517b6119b3355558
Sydney Erickson <sydneyerickson@icloud.com>
2018-05-31 14:30:51 -0700

    iso646.h

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ A prototype C library for SydOS designed to be up to the C99 standard.
 ### Currently (supposedly) conforming headers:
 
 ```
-assert.h
+assert.h iso646.h
 ```
 
 ### Currently in progress headers:
 ```
-ctype.h errno.h iso646.h limits.h
+ctype.h errno.h limits.h
 ```
---
+++ src/include/iso646.h
@@ -0,0 +1,18 @@
+// Defines some plain text words for use in comparasions
+
+#ifndef _ISO646_H_
+#define	_ISO646_H_
+
+#define and    &&
+#define and_eq &=
+#define bitand &
+#define bitor  |
+#define compl  ~
+#define not    !
+#define not_eq !=
+#define or     ||
+#define or_eq  |=
+#define xor    ^
+#define xor_eq ^=
+
+#endif
