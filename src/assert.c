c47c5c1b8849936ff7475e85a0954a1a2eb394c8
Sydney Erickson <sydneyerickson@icloud.com>
2018-05-31 13:38:37 -0700

    Assert comments

--- src/assert.c
+++ src/assert.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Internal assertion defintition
 void __assert(const char *message, const char* function, const char* file, 
 			  int line) {
+	// Print debug line
 	printf("Assertion failed: %s, function %s, file %s, line %u.\n"
 		   "Application will now terminate.\n", message, function, file, line);
 	//TODO: Convert exit() to abort()
+	// Terminate program
 	exit(-1);
 }
--- src/include/assert.h
+++ src/include/assert.h
@@ -1,6 +1,15 @@
+// Assert.h defines the assert macro and internal function. This is used to
+// verify an expression is true (such as 4+4 = 8). It kills the app if the
+// evaluated expression is false. This is mostly used to make sure sane values
+// are sent along in debugging or mission critial stuff
+
 #ifndef _ASSERT_H
 #define _ASSERT_H
 
+// Check if N(o)DEBUG is a defined macro, if it is place a stub so assert
+// doesn't trigger. Otherwise, define a macro to call our internal function.
+// We don't do assert as a function itself since a marco can automatically grab
+// the data we need to conform to the C99 standard
 #if defined (NDEBUG)
 #define assert(expression) ((void)0)
 #else
@@ -8,6 +17,7 @@
 								   __func__, __FILE__, __LINE__), 0))
 #endif
 
+// Internal assertion function definition
 extern void __assert(const char *message, const char* function, 
 					 const char* file, int line);
 
