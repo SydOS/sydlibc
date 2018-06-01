8897e9498ea79ee9d33b9fb4c905ccdc0a3e18c2
Sydney Erickson <sydneyerickson@icloud.com>
2018-06-01 08:34:44 -0700

    WIP limits.h

---
+++ src/include/limits.h
@@ -0,0 +1,22 @@
+#define  CHAR_BIT    8
+#define SCHAR_MIN  (-128)
+#define SCHAR_MAX    127
+#define UCHAR_MAX    255
+#define  CHAR_MIN  (-128)
+#define  CHAR_MAX    127
+
+#define MB_LEN_MAX   4
+
+#define  SHRT_MIN  (−32768)
+#define  SHRT_MAX    32767
+#define USHRT_MAX    65535
+
+#define  INT_MIN   (−2147483648)
+#define  INT_MAX     2147483647
+#define UINT_MAX     4294967295
+
+//TODO: LONGs
+
+#define  LLONG_MIN (-9223372036854775808)
+#define  LLONG_MAX   9223372036854775807
+#define ULLONG_MAX   18446744073709551615
