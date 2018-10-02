ed6c6d75731a8a7a95805cda730c032ae6b20c84
Sydney Erickson <sydneyerickson@icloud.com>
2018-10-02 10:58:59 -0700

    ctype - better isdigit

--- src/ctype.c
+++ src/ctype.c
@@ -1,13 +1,3 @@
-const char numbers[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-
 int isdigit(int c) {
-	// Check if the passed char is one of these numbers
-	for (int i = 0; i < sizeof(numbers); i++) {
-		if (c == numbers[i]) {
-			return 1;
-		}
-	}
-
-	// If we get here, it is not one of these numbers
-	return 0;
+	return c >= '0' && c <= '9';
 }
