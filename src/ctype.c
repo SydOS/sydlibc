9970e963136bccda258836fb21e69f7e5153e85b
Sydney Erickson <sydneyerickson@icloud.com>
2019-01-22 10:13:57 -0800

    ctype - add isalpha and isalnum

--- src/ctype.c
+++ src/ctype.c
@@ -24,3 +24,21 @@ int islower(int c) {
 int isupper(int c) {
 	return c >= 'A' && c <= 'Z';
 }
+
+/**
+ * Function to check if the passed char is an alpha character
+ * @param a char
+ * @return 1 if is an alpha character, 0 if not
+ */
+int isalpha(int c) {
+	return isupper(c) || islower(c);
+}
+
+/**
+ * Function to check if the passed char is alphanumeric
+ * @param a char
+ * @return 1 if is an alphanumeric character, 0 if not
+ */
+int isalnum(int c) {
+	return isalpha(c) || isdigit(c);
+}
